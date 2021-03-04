2c58bbc255219d01c3a086a6dcd7eff9de78c7f1
laila <lailahajjamy@gmail.com>
2021-03-04 02:43:02 +0100

    laila

--- src/app/adduser/adduser.page.ts
+++ src/app/adduser/adduser.page.ts
@@ -13,20 +13,33 @@ import { Observable } from 'rxjs';
 })
 export class AdduserPage implements OnInit {
   usersRef:  AngularFireList<any>;
+  usersObjectRef: AngularFireObject<any>;
   users: Observable<any>;
   formUserAdd : FormGroup;
   nom : string ;
   prenom : string ;
   poste : string ;
   carteId : string ;
-  public autorised : boolean ;
+  public autorised : boolean;
   public checkedState: boolean;
+
+
+  messagePoste = 'le champs <strong>Poste</strong> doit contenir seulement des lettres'
+  messageChamps = 'Veuillez remplir <strong>tous</strong> les champs'
+  messageCarteId = 'le champs <strong>Carte ID </strong> est invalide'
+  messageNom = 'le champs <strong>Nom</strong> doit contenir seulement des lettres'
+  messagePrenom ='le champs <strong>Prénom</strong> doit contenir seulement des lettres'
+  
+  validatedNom : boolean = true;
+  validatedPrenom : boolean = true;
+  validatedPoste : boolean = true;
+
   constructor(public toastController: ToastController,public db: AngularFireDatabase,public alertController: AlertController, public formBuilder : FormBuilder, public navCtrl: NavController,private authService : AuthService) 
   {
 
 
-    this.usersRef = db.list('users');
-    this.users = this.usersRef.valueChanges();
+ 
+    // this.users = this.usersRef.valueChanges();
     this.autorised= true;
     this.checkedState = true;
 
@@ -41,7 +54,7 @@ export class AdduserPage implements OnInit {
         Validators.required
       ])),
       carteId : new FormControl('', Validators.required),
-     autorised : new FormControl('', Validators.compose([
+      autorised : new FormControl('', Validators.compose([
         Validators.required
    ]))
 });
@@ -64,8 +77,11 @@ save(){
    autorised:this.autorised
             
  };
- this.usersRef.push(userData);
-  
+
+    this.usersRef = this.db.list(`/users/${this.carteId}`);
+    this.usersRef.push(userData);
+    this.usersObjectRef = this.db.object(`/users/${this.carteId}`);
+    this.usersObjectRef.set(userData);
   
 }
 
@@ -73,60 +89,83 @@ save(){
 
 
 
-ajouter(){
+async ajouter(){
 
   var carteIdSplit:string[]
   
     if(this.nom!=null && this.prenom!=null && this.carteId!=null && this.poste!=null ){
+
+      if(/^[a-zA-Z ]*$/.test(this.nom)==false){
+       this.toast(this.messageNom)
+       this.validatedNom=false;
+      }
+
+      
+      if(/^[a-zA-Z ]*$/.test(this.prenom)==false){
+        this.toast(this.messagePrenom)
+        this.validatedPrenom=false;
+      }
+
+
+      if(/^[a-zA-Z ]*$/.test(this.poste)==false){
+        this.toast(this.messagePoste)
+        this.validatedPoste=false;
+      }
+
     
       carteIdSplit=this.carteId.split(" ");
       if(carteIdSplit.length==4)
       {
     
           if(carteIdSplit[0].length!=2 || carteIdSplit[1].length!=2 || carteIdSplit[2].length!=2 || carteIdSplit[3].length!=2){
-            this.toast();
+            this.toast(this.messageCarteId);
+           
             
           }
           else{
                 if (/^([A-Z]+)$/.test(carteIdSplit[0])==false || /^([0-9]+)$/.test(carteIdSplit[1])==false || /^([0-9]+)$/.test(carteIdSplit[2])==false || /^([0-9][A-Z]+)$/.test(carteIdSplit[3])==false ){
                         
                   
-                  this.toast();
-                  
+                  this.toast(this.messageCarteId);                            
                 }
     
                 else{
-                
-                  this.save();
-    
-                }
-    
-    
-    
+                  if(this.validatedNom && this.validatedPrenom && this.validatedPoste){
+                    this.save();
+                    this.navCtrl.navigateRoot('users');
+                    const toast = await this.toastController.create({
+                      message: "Utilisateur créé avec succés",
+                      position: 'top',
+                      color:'success',
+                      duration: 2000
+                    });
+                    toast.present();
+                  }
+                  
+                 
+                }   
               }   
     
       }else
           {
-            this.toast(); 
-          }
-     
-    
+            this.toast(this.messageCarteId);
+           
+          }    
     }
   
    
     else{
   
-      this.toastChamps();
+      this.toast(this.messageChamps);
   
       }
   
   
     }
   
-  
-  async toast(){
+  async toast(message){
     const toast = await this.toastController.create({
-      message: 'le champs <strong>ID carte</strong> est invalid',
+      message: message,
       position: 'top',
       color:'warning',
       duration: 2000
@@ -136,16 +175,7 @@ ajouter(){
   }
   
   
-  async toastChamps(){
-    const toast = await this.toastController.create({
-      message: 'Veuillez remplir <strong>tous</strong> les champs',
-      position: 'top',
-      color:'warning',
-      duration: 2000
-    });
-    toast.present();
-  
-  }
+
   
   
   
--- src/app/users/users.page.ts
+++ src/app/users/users.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertController,ToastController } from '@ionic/angular';
+import { AngularFireDatabase, AngularFireObject, AngularFireList, snapshotChanges } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -9,10 +13,44 @@ import { AuthService } from '../services/auth.service';
 })
 export class UsersPage implements OnInit {
 
-  constructor(private authService: AuthService,public navCtrl: NavController) { }
+  usersListRef: AngularFireList<any>;
+  Users = [];
+  constructor(public toastController: ToastController,public db: AngularFireDatabase,public alertController: AlertController, public formBuilder : FormBuilder, public navCtrl: NavController,private authService : AuthService) 
+  {
+
+
+
+  }
 
   ngOnInit() {
+    this.fetchUsers();
+    let usersRes = this.getUsersList();
+    usersRes.snapshotChanges().subscribe(res => {
+      this.Users = [];
+      res.forEach(item => {
+        let a = item.payload.toJSON();
+        a['$key'] = item.key;
+        this.Users.push(a);
+      })
+    })
   }
+
+
+
+  getUsersList() {
+    this.usersListRef = this.db.list('/users');
+    return this.usersListRef;
+  }
+
+
+  fetchUsers() {
+    this.getUsersList().valueChanges().subscribe(res => {
+      console.log(res)
+    })
+  }
+
+
+
 
   lastactivityPage(){
     this.navCtrl.navigateForward('lastactivity');
@@ -28,6 +66,8 @@ export class UsersPage implements OnInit {
     this.navCtrl.navigateForward('adduser');
 
   }
+
+
 
 
 
