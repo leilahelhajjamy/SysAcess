1b60bfd08ab6a6c703546f4400ff8c121d7de314
laila <lailahajjamy@gmail.com>
2021-04-13 18:26:06 +0200

    regular pattern

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DatePicker } from '@ionic-native/date-picker/ngx';
 import { FCMNG } from 'fcm-ng';
 import { firebaseConfig } from '../../config/config.js';
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyBgN5HXli1TiiCZney2RV25spifnhLbac0',
-//   authDomain: 'sysaccess-ecf6b.firebaseapp.com',
-//   databaseURL: 'https://sysaccess-ecf6b-default-rtdb.firebaseio.com',
-//   projectId: 'sysaccess-ecf6b',
-//   storageBucket: 'sysaccess-ecf6b.appspot.com',
-//   messagingSenderId: '879760335132',
-//   appId: '1:879760335132:web:22808658174a344135dad9',
-//   measurementId: 'G-982KE3TNTJ',
-// };
 
 @NgModule({
   declarations: [AppComponent],
